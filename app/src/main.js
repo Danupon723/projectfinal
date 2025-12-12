8f210d9308a2efd46a123a27e00585a8aada93e7
Danupon723 <danuponmaneesing@gmail.com>
2025-12-12 10:59:45 +0700

    v

---
+++ app/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import 'vuetify/styles'
+import App from './App.vue'
+import { createVuetify } from 'vuetify'
+import '@mdi/font.css/materialdesignicons.css'
+
+createApp(App).mount('#app')
